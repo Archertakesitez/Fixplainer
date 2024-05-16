7e696bdf04a287ee8e77c1e3e64bb0fc9a7cf0a5
Erchi Zhang <erchizhang@brandeis.edu>
2024-05-16 00:52:00 -0400

    relocate pngs

--- run/main.py
+++ run/main.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     """Main function for yielding the GUI window and explanations.
 
     Args:
-        arguments 1: image path
-        arguments 2 (optional): the scale you want the image to show on your screen
+        argument 1: image path
+        argument 2: number of inter-objects occlusion 
+        argument 3 (optional): the scale in which you want your image to show on the screen
+        argument 4 (optional): SHAP plot type to be generated
     """
     make_interface()

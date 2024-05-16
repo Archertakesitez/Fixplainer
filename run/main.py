0935e2a64ce6313fc204a0d9304ce24cf9783c97
Erchi Zhang <erchizhang@brandeis.edu>
2024-05-16 02:31:33 -0400

    dbg

--- run/main.py
+++ run/main.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     Args:
         argument 1: image path
         argument 2: number of inter-objects occlusion 
-        argument 3 (optional): the scale in which you want your image to show on the screen
-        argument 4 (optional): SHAP plot type to be generated
+        --scale (optional): the scale in which you want your image to show on the screen
+        --plot_type (optional): SHAP plot type to be generated
+        --model_path (optional): the pretrained model you want to use
+        --X_train_path (optional): the X_train set corresponding to your pretrained model
     """
     make_interface()
